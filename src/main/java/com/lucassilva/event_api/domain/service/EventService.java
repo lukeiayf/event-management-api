3473c332d26ef24d66f0230400419b672224fa58
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-09-18 15:37:01 -0300

    feat: adds service class and aws configuration

--- src/main/java/com/lucassilva/event_api/domain/service/EventService.java
+++ src/main/java/com/lucassilva/event_api/domain/service/EventService.java
@@ -1,14 +1,28 @@
 package com.lucassilva.event_api.domain.service;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.lucassilva.event_api.domain.event.Event;
 import com.lucassilva.event_api.domain.event.EventRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class EventService {
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
     public Event createEvent(EventRequestDTO data) {
         String imgUrl = null;
 
@@ -26,8 +40,25 @@ public class EventService {
         return newEvent;
     }
 
-    private String uploadImg(MultipartFile file) {
-        return "";
+    private String uploadImg(MultipartFile multipartFile) {
+        String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+        try {
+            File file = this.convertMultipartToFile(multipartFile);
+            s3Client.putObject(bucketName, fileName, file);
+            file.delete();
+            return s3Client.getUrl(bucketName, fileName).toString();
+        } catch (Exception e) {
+            System.out.println("Error uploading file, message: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private File convertMultipartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
     }
 
 
