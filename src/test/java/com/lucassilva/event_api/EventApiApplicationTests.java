5de0b65371a2a9fc03cdb4e2d0bab60c411503e5
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-09-19 16:59:06 -0300

    test: adds test configuration

--- src/test/java/com/lucassilva/event_api/EventApiApplicationTests.java
+++ src/test/java/com/lucassilva/event_api/EventApiApplicationTests.java
@@ -2,12 +2,14 @@ package com.lucassilva.event_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
